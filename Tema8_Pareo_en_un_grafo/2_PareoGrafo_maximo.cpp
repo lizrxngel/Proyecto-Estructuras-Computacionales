b79e5c3f98a74189591dc1e734e27cbce310826a
alexdelgadillo-arch <alejandrodelgadordzzz@gmail.com>
2025-12-03 16:55:12 -0600

    Implementación del algoritmo hungaro para el pareo

--- Tema8_Pareo_en_un_grafo/2_PareoGrafo_maximo.cpp
+++ Tema8_Pareo_en_un_grafo/2_PareoGrafo_maximo.cpp
@@ -6,6 +6,64 @@ using namespace std;
 // Algoritmo Hungaro para costo minimo de asignacion en matriz cuadrada
 // Devuelve el emparejamiento y el costo total minimo.
 
+pair<vector<int>, int> hungaroMinimo(const vector<vector<int>> &costo) {
+    int n = (int)costo.size();
+    const int INF = numeric_limits<int>::max();
+    vector<int> u(n + 1), v(n + 1), p(n + 1), way(n + 1);
+
+    for (int i = 1; i <= n; ++i) {
+        p[0] = i;
+        int j0 = 0;
+        vector<int> minv(n + 1, INF);
+        vector<bool> usado(n + 1, false);
+        do {
+            usado[j0] = true;
+            int i0 = p[j0], delta = INF, j1 = 0;
+            for (int j = 1; j <= n; ++j) {
+                if (!usado[j]) {
+                    int cur = costo[i0 - 1][j - 1] - u[i0] - v[j];
+                    if (cur < minv[j]) {
+                        minv[j] = cur;
+                        way[j] = j0;
+                    }
+                    if (minv[j] < delta) {
+                        delta = minv[j];
+                        j1 = j;
+                    }
+                }
+            }
+            for (int j = 0; j <= n; ++j) {
+                if (usado[j]) {
+                    u[p[j]] += delta;
+                    v[j] -= delta;
+                } else {
+                    minv[j] -= delta;
+                }
+            }
+            j0 = j1;
+        } while (p[j0] != 0);
+        do {
+            int j1 = way[j0];
+            p[j0] = p[j1];
+            j0 = j1;
+        } while (j0 != 0);
+    }
+
+    vector<int> asignacion(n, -1);
+    for (int j = 1; j <= n; ++j) {
+        if (p[j] != 0) {
+            asignacion[p[j] - 1] = j - 1;
+        }
+    }
+
+    int costoTotal = 0;
+    for (int i = 0; i < n; ++i) {
+        costoTotal += costo[i][asignacion[i]];
+    }
+    return {asignacion, costoTotal};
+}
+
+
 int main() {
 
     int opcion;
